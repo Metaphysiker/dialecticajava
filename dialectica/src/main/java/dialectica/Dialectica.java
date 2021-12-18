634142b35b781a827ee2a1bb4f063c2cdfcbe611
Metaphysiker <s.raess@me.com>
2021-12-18 09:36:55 +0100

    initial commit

---
+++ dialectica/src/main/java/dialectica/Dialectica.java
@@ -0,0 +1,11 @@
+package dialectica;
+
+public class Dialectica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Dialectica Start");
+
+	}
+
+}
